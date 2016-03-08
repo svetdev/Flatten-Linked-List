69bce8ebf2846b55a0818a1103e75345ccc9d439
Andrey Kasatkin <akasatkin@coocoo.com>
2016-03-07 19:20:42 -0500

    Initial Commit

---
+++ FlattenLinkedList/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  FlattenLinkedList
+//
+//  Created by Andrey Kasatkin on 3/7/16.
+//  Copyright © 2016 Svetliy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
